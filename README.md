4c6c90edc34b53bf7ab2ef97320fdcab10a496de
Uğur Büyükyılmaz <ugur.buyukyilmaz@gmail.com>
2024-06-02 01:04:57 +0300

    1.0.1

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ implementation("com.github.buyukyilmaz:tehanu:1.0.0")
 |-------------------|--------|
 | compileSdk        | 34     |
 | buildToolsVersion | 34.0.0 |
-| minSdk            | 27     |
+| minSdk            | 28     |
 | kotlinVersion     | 1.9.24 |
 | gradleVersion     | 8.4.1  |
 | jvmToolchain      | 17     |
@@ -36,7 +36,6 @@ implementation("com.github.buyukyilmaz:tehanu:1.0.0")
 implementation("androidx.compose:compose-bom:2024.05.00")
 implementation("androidx.compose.ui:ui")
 implementation("androidx.compose.foundation:foundation")
-implementation("androidx.compose.material3:material3")
 implementation("androidx.compose.runtime:runtime-livedata")
 implementation("androidx.activity:activity-compose:1.9.0")
 implementation("androidx.appcompat:appcompat:1.7.0")
@@ -45,8 +44,6 @@ implementation("com.google.android.material:material:1.12.0")
 implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 implementation("com.google.code.gson:gson:2.11.0")
 implementation("androidx.datastore:datastore-preferences:1.1.1")
-implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 implementation("androidx.navigation:navigation-compose:2.7.7")
-implementation("io.coil-kt:coil-compose:2.6.0")
 implementation("androidx.work:work-runtime-ktx:2.9.0")
 ```
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     namespace = "com.glorfindel.tehanu"
     compileSdk = 34
     buildToolsVersion = "34.0.0"
-    defaultConfig.minSdk = 27
+    defaultConfig.minSdk = 28
     kotlin.jvmToolchain(17)
     buildFeatures.compose = true
     composeOptions.kotlinCompilerExtensionVersion = "1.5.14"
@@ -27,7 +27,6 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.foundation:foundation")
-    implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.runtime:runtime-livedata")
 
     implementation("androidx.activity:activity-compose:1.9.0")
@@ -37,9 +36,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("androidx.work:work-runtime-ktx:2.9.0")
 }
 
@@ -50,7 +47,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.glorfindel.tehanu"
                 artifactId = "tehanu"
-                version = "1.0.0"
+                version = "1.0.1"
             }
         }
     }
